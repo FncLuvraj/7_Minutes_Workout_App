935217b2bd31f81810f63561aef8bfc8949fe8f0
Luvraj Singh <98079619+FncLuvraj@users.noreply.github.com>
2022-10-20 11:03:54 +0530

    Update HistoryAdapter.kt

--- app/src/main/java/luvraj/app/a7_minutesworkoutapp/HistoryAdapter.kt
+++ app/src/main/java/luvraj/app/a7_minutesworkoutapp/HistoryAdapter.kt
@@ -4,7 +4,7 @@ import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import eu.tutorials.a7_minutesworkoutapp.databinding.ItemHistoryRowBinding
+import luvraj.app.a7_minutesworkoutapp.databinding.ItemHistoryRowBinding
 
 // TODO(Step 2 : Created a adapter class to bind the to RecyclerView to show the list of completed dates in History Screen.)
 // START
